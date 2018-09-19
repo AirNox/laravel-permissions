3815dbd47e1d97cc3078f3832cfaf5457908c20a
Krzysztof <krzysztof@engineer.com>
2018-09-19 21:46:59 +0200

    Test suite

---
+++ src/PermissionsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Airnox\Permissions;
+
+use Illuminate\Support\ServiceProvider;
+
+class PermissionsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        
+    }
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
